bc2589dd1a61e8fbba59a2a91e60087da73a35c7
Fak <facundo.g.becerra@gmail.com>
2022-10-07 20:28:33 +0100

    Agrego operadores

---
+++ 02_operators.py
@@ -0,0 +1,21 @@
+### Operadores ###
+
+print(3 + 4)
+print(3 - 4)
+print(3 * 4)
+print(3 / 4)
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 3)
+
+
+print("Hola " + "Python")
+
+
+print(3 > 4)
+print(3 < 4)
+print(3 <= 4)
+print(3 >= 4)
+print(3 == 4)
+print(3 != 4)
