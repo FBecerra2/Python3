39e1c711f2649fbfb625242e5316a5094e57da2e
Fak <facundo.g.becerra@gmail.com>
2022-10-10 21:11:14 +0100

    56

---
+++ 04_list.py
@@ -0,0 +1,18 @@
+my_list = list()
+my_other_list = []
+
+name = "Perro"
+print(len(my_list))
+
+my_list = [35, 24, 62, 52, 30, 30, 17]
+
+
+print(len(my_list))
+print(my_list)
+
+my_other_list = [35, 1.77, "Fak", "Pepito", name]
+
+print(my_other_list)
+print(type(my_other_list))
+
+print(my_list[6])
